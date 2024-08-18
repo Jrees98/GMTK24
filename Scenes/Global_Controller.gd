378cc39dbdc946c7364a4d32123d4a8531182357
Jacob Rees <reescodes@gmail.com>
2024-08-18 11:24:15 -0600

    add global_controller to update labels

---
+++ Scenes/Global_Controller.gd
@@ -0,0 +1,16 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$"../Lemonade".text = str(Global.cups_of_lemonade)
+	$"../Dollars".text = str(Global.dollars)
+	$"../Shop/Lemons/InventoryItem/ItemQuantity".text = str(Global.lemons)
+	$"../Shop/Cups/InventoryItem/ItemQuantity".text = str(Global.cups)
+	$"../Shop/Ice/InventoryItem/ItemQuantity".text = str(Global.ice)
+	$"../Shop/Sugar/InventoryItem/ItemQuantity".text = str(Global.sugar)
